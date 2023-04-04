eb9e6bd529641baa9365f9840e6cbf35771dfa2c
caio.villela <caio.villela@hotmail.com>
2023-04-04 05:06:49 -0300

    [feat] exception treatment, automated posting, model update

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from sklearn.preprocessing import StandardScaler
 from transformers import pipeline
 import torch
 import argparse
+import time
 
 from constants import specialty_labels, category_labels, industry_labels, tech_labels, tech_stack_dict
 
@@ -14,8 +15,11 @@ app = Flask(__name__)
 
 
 def preprocess(data):
+    # exclude type
+    del data['type']
+
     # data to df - preprocessing made easier
-    df = pd.DataFrame.from_dict(data, orient='columns')
+    df = pd.DataFrame(data, index=[0])
 
     # categorical variables
     # funding, events, hubs
@@ -32,6 +36,9 @@ def preprocess(data):
                           'linkedin_url', 'overview', 'sic_codes', 'company_hubs', 'events'])
 
     # size
+    if pd.isnull(df['size'].loc[0]):
+        df['size'] = '0-1'
+
     df['size'] = df['size'].str.replace(r'[^0-9\-]+', '')
     df.loc[df['size'] == '10001', 'size'] = '500+'
     df.loc[df['size'] == '5001-10000', 'size'] = '500+'
@@ -77,32 +84,30 @@ def preprocess(data):
 
     # preprocess text data using Transformers
     # This step is probably the slowest on the preprocessing pipeline, and could be optimized in the future
-
-    df[industry_labels] = 0
-    df[category_labels] = 0
-    df[tech_labels] = 0
     df[specialty_labels] = 0
-
-    if df['specialties'] is not None:
-        cat = df['specialties']
+    if not pd.isnull(df['specialties'].loc[0]):
+        cat = df['specialties'].loc[0]
         predicted_category = classifier(cat, candidate_labels=specialty_labels)
         for label, score in zip(predicted_category['labels'], predicted_category['scores']):
             df[label] = score
 
-    if df['categories'] is not None:
-        cat = df['categories']
+    df[category_labels] = 0
+    if not pd.isnull(df.loc[0]['categories']):
+        cat = df['categories'].loc[0]
         predicted_category = classifier(cat, candidate_labels=category_labels)
         for label, score in zip(predicted_category['labels'], predicted_category['scores']):
             df[label] = score
 
-    if df['industry'] is not None:
-        cat = df['industry']
+    df[industry_labels] = 0
+    if not pd.isnull(df['industry'].loc[0]):
+        cat = df['industry'].loc[0]
         predicted_category = classifier(cat, candidate_labels=industry_labels)
         for label, score in zip(predicted_category['labels'], predicted_category['scores']):
             df[label] = score
 
-    if df['technologies'] is not None:
-        techs = df['technologies'].split(',')
+    df[tech_labels] = 0
+    if not pd.isnull(df['technologies'].loc[0]):
+        techs = df['technologies'].loc[0].split(',')
         for tech in techs:
             if tech in tech_stack_dict.keys():
                 for cat in tech_stack_dict[tech]:
@@ -111,7 +116,6 @@ def preprocess(data):
     # scale text features
     cols_to_scale = category_labels + industry_labels + specialty_labels
     scaler = joblib.load('models/txt_std_scaler.joblib')
-    cols_to_scale = ['employees_on_linkedin', 'followers', 'founded']
     df[cols_to_scale] = scaler.transform(df[cols_to_scale])
 
     df_pp = df.drop(
@@ -122,24 +126,52 @@ def preprocess(data):
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Get the request data as a dictionary
-    data = request.get_json()
+    start = time.time()
+    print('/predict request')
 
-    X = preprocess(data)
+    try:
+        data = request.get_json(force=True)
+        # Verify that all required fields are present in the request
+        required_fields = {'name', 'alexa_rank', 'city', 'state', 'country', 'hq', 'website', 'employees_on_linkedin',
+                           'followers', 'founded', 'industry', 'linkedin_url', 'overview', 'ownership_type',
+                           'sic_codes', 'size', 'specialties', 'total_funding', 'technologies', 'company_hubs',
+                           'events', 'categories'}
+        if not all(field in data for field in required_fields):
+            raise ValueError('Missing one or more required fields')
+
+        # Your code for prediction and response generation goes here...
+
+    except ValueError as e:
+        # Handle the case where one or more required fields are missing
+        error_response = {'error': str(e)}
+        return jsonify(error_response), 400
+
+    except Exception as e:
+        # Handle any other exceptions that might occur
+        error_response = {'error': 'Internal Server Error'}
+        return jsonify(error_response), 500
+
+    X_request = preprocess(data)
 
     # Make a prediction using the loaded model
-    y_b2c = model_b2c.predict(X)
-    print(y_b2c)
+    y_b2c = model_b2c.predict(X_request)
+    y_b2b = model_b2b.predict(X_request)
 
-    y_b2b = model_b2b.predict(X)
-    print(y_b2b)
+    y_b2c = y_b2c[np.argmax(y_b2c)]
+    y_b2b = y_b2b[np.argmax(y_b2b)]
 
     # Convert the prediction to a dictionary and return as JSON
-    output = {'prediction_b2c': int(y_b2c), 'prediction_b2b': int(y_b2b) }
+    output = {'prediction_b2c': int(y_b2c), 'prediction_b2b': int(y_b2b)}
+    end = time.time()
+
+    print('Prediction made in {} seconds'.format(end-start))
     return jsonify(output)
 
 
 if __name__ == '__main__':
+
+    start = time.time()
+    print('Initializing environment . . .')
 
     device_id = torch.cuda.current_device() if torch.cuda.is_available() else -1
     print('running on device = {}'.format(device_id))
@@ -149,15 +181,19 @@ if __name__ == '__main__':
     args = parser.parse_args()
     if args.load_pretrained_bart:
         # simply load pretrained downloaded model.
+        print("loading local classifier from './bart-classifier/' . . .")
         classifier = pipeline(task='zero-shot-classification', model='bart-classifier')
     else:
         # download BART model. This step may take long depending on computational capacity [1.63GB model] and it is
         # recomended to already have the BART model preinstalled on your machine
+        print('downloading classifier from Huggingface...')
         classifier = pipeline(model="facebook/bart-large-mnli", device=device_id)
 
+    print('loading pretrained lgbm models . . .')
     # Load the LightGBM models
     model_b2b = pickle.load(open("models/b2b_model.pkl", "rb"))
     model_b2c = pickle.load(open("models/b2c_model.pkl", "rb"))
 
+    print("Service started in {} seconds.".format(time.time()-start))
     app.run(port=5000, debug=True)
 
--- test_post.py
+++ test_post.py
@@ -1,29 +1,34 @@
 import requests
 import json
 import pandas as pd
-import random
+import numpy as np
 
 # load the dataframe
 df = pd.read_csv('data.csv')
 
-# select a random row from the dataframe
-row = df.sample().iloc[0]
+while True:
+    # select a random row from the dataframe
+    row = df.sample().iloc[0]
+    actual_type = row['type']
+    print('Using row {}'.format(row.name))
+    print('Actual Type: {}'.format(actual_type))
 
-# convert the row to a dictionary
-data = row.to_dict()
 
-# convert the dictionary to a JSON string
-json_data = json.dumps(data)
-print(json_data)
+    # convert the row to a dictionary
+    data = row.drop(columns=['type']).to_dict()
 
-# set the URL for the Flask application
-url = 'http://localhost:5000/predict'
+    # convert the dictionary to a JSON string
+    json_data = json.dumps(data)
+    print(json_data)
 
-# set the headers for the POST request
-headers = {'Content-type': 'application/json'}
+    # set the URL for the Flask application
+    url = 'http://localhost:5000/predict'
 
-# send the POST request
-response = requests.post(url, data=json_data, headers=headers)
+    # set the headers for the POST request
+    headers = {'Content-type': 'application/json'}
 
-# print the response from the Flask application
-print(response.text)
+    # send the POST request
+    response = requests.post(url, data=json_data, headers=headers)
+
+    # print the response from the Flask application
+    print(response.text)
